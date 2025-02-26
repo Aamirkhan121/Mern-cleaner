9649e126ba51bc5ac3aa2be03230ca81455a4730
Aamirkhan121 <131248243+Aamirkhan121@users.noreply.github.com>
2025-02-26 09:53:54 +0530

    Update db.js

--- backend/dist/db.js
+++ backend/dist/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const mongodbUri = process.env.MONGODB_URI;
 const Connected =async()=>{
     try {
-        await mongoose.connect(mongodbUri)
+        await `mongoose.connect`(mongodbUri)
         console.log('Connected to MongoDB');
     } catch (error) {
         console.log('Error connecting to MongoDB', error.message);
